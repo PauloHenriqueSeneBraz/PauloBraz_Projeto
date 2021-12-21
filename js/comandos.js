6bed94299b17c03eea24ce2ea2b135dbb94dbba2
Paulo henrique sene Braz <63761476+PauloHenriqueSeneBraz@users.noreply.github.com>
2021-12-21 16:39:32 -0300

    Primeira atividade TCS
    Adicionei as páginas principais, estilos e comandos JS

---
+++ js/comandos.js
@@ -0,0 +1,11 @@
+function carHome() {
+    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/index.html"
+}
+
+function carPes() {
+    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/pessoal.html"
+}
+
+function carForm() {
+    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/form.html"
+}
