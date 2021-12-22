f7e70a81d03b728851c75d5b7f57d6e9f846d454
Paulo henrique sene Braz <63761476+PauloHenriqueSeneBraz@users.noreply.github.com>
2021-12-22 12:04:49 -0300

    Realizei alterações no formulário e adicionei minhas redes sociais

--- js/comandos.js
+++ js/comandos.js
@@ -1,11 +1,38 @@
 function carHome() {
-    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/index.html"
+    window.location.href = "index.html"
 }
 
 function carPes() {
-    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/pessoal.html"
+    window.location.href = "pessoal.html"
 }
 
 function carForm() {
-    window.location.href = "file:///C:/Users/Eu/Desktop/PauloBraz_Projeto/form.html"
+    window.location.href = "form.html"
+}
+
+function validarform() {
+    var nome = form.nome.value;
+    var email = form.email.value;
+    var mensagem = form.mensagem.value;
+
+    if (nome == "" || nome.indexOf(" ")) {
+        alert("Digite seu nome corretamente");
+        form.nome.focus();
+        return false;
+    }
+
+    if (email == "" || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+        alert("Email obrigatorio!");
+        form.email.focus();
+        return false;
+    }
+
+    if (mensagem == "" || mensagem < 18) {
+        alert("Mensagem obrigatoria!");
+        form.mensagem.focus();
+        return false;
+    }
+
+    alert("Mensagem enviada!");
+
 }
